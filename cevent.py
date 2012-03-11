b91d1444f5d8849f52e5491a03ec69357927c627
Przemys?aw Lib <an.przemek@gmail.com>
2012-03-11 16:45:10 +0100

    Added missing on_event method to CEvent

--- cevent.py
+++ cevent.py
@@ -58,7 +58,61 @@ class CEvent:
     def on_joy_ball(self,event):
         pass
     def on_event(self, event):
-    pass
+        pass
+    def on_event(self, event):
+        if event.type == QUIT:
+            self.on_exit()
+ 
+        elif event.type >= USEREVENT:
+            self.on_user(event)
+ 
+        elif event.type == VIDEOEXPOSE:
+            self.on_expose()
+ 
+        elif event.type == VIDEORESIZE:
+            self.on_resize(event)
+ 
+        elif event.type == KEYUP:
+            self.on_key_up(event)
+ 
+        elif event.type == KEYDOWN:
+            self.on_key_down(event)
+ 
+        elif event.type == MOUSEMOTION:
+            self.on_mouse_move(event)
+ 
+        elif event.type == MOUSEBUTTONUP:
+            if event.button == 0:
+                self.on_lbutton_up(event)
+            elif event.button == 1:
+                self.on_mbutton_up(event)
+            elif event.button == 2:
+                self.on_rbutton_up(event)
+ 
+        elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 0:
+                self.on_lbutton_down(event)
+            elif event.button == 1:
+                self.on_mbutton_down(event)
+            elif event.button == 2:
+                self.on_rbutton_down(event)
+ 
+        elif event.type == ACTIVEEVENT:
+            if event.state == 1:
+                if event.gain:
+                    self.on_mouse_focus()
+                else:
+                    self.on_mouse_blur()
+            elif event.state == 2:
+                if event.gain:
+                    self.on_input_focus()
+                else:
+                    self.on_input_blur()
+            elif event.state == 4:
+                if event.gain:
+                    self.on_restore()
+                else:
+                    self.on_minimize()
  
 if __name__ == "__main__" :
     event = CEvent()
