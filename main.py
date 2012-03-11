59f0523deaec34548415965cc6ea5d2f836d630c
Przemys?aw Lib <an.przemek@gmail.com>
2012-03-11 16:40:28 +0100

    Events tutorial

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 import pygame
 from pygame.locals import *
- 
-class App:
+import cevent
+
+class App(cevent.CEvent):
     def __init__(self):
         self._running = True
         self._display_surf = None
         self._image_surf = None
- 
+    
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode((350,350), pygame.HWSURFACE)
         self._running = True
         self._image_surf = pygame.image.load("myimage.jpg").convert()
- 
-    def on_event(self, event):
-        if event.type == QUIT:
-            self._running = False
+    
     def on_loop(self):
         pass
     def on_render(self):
         self._display_surf.blit(self._image_surf,(0,0))
         pygame.display.flip()
     
+    def on_exit(self):
+        self._running = False
+
     def on_cleanup(self):
         pygame.quit()
- 
+     
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
- 
+    
         while( self._running ):
             for event in pygame.event.get():
                 self.on_event(event)
